f36a1e3e17c5c899b5499643e9c040447d979c13
iris5105 <118243282+iris5105@users.noreply.github.com>
2023-01-19 16:23:22 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12800 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+	Scanner in = new Scanner(System.in);
+	String ID = in.nextLine();
+	System.out.print(ID + "??!");
+	}
+	}
