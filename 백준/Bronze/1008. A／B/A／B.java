3efc9df651f7e01dd828ee0b7987d1aa0a6f6528
iris5105 <118243282+iris5105@users.noreply.github.com>
2023-01-19 15:05:11 +0900

    [Bronze V] Title: A/B, Time: 112 ms, Memory: 12940 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+       
+       int A = in.nextInt();
+       int B = in.nextInt();
+        System.out.print(A/(double)B);
+    }
+}
