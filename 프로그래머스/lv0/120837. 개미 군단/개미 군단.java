93073ade4fea2eb10b2f645de3431e291565eb55
iris5105 <118243282+iris5105@users.noreply.github.com>
2023-01-22 22:34:38 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        if(hp%5==0){
+            answer=hp/5;
+        }else if((hp%5)==3){
+            answer=hp/5+hp%5/3;
+        }else{
+            answer=hp/5+(hp%5/3)+((hp%5)%3);
+        }
+        return answer;
+    }
+}
